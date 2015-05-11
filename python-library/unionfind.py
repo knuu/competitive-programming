745b3f2c42d926018bb53960ca689f5ae9658ba8
knuu <premier3next@yahoo.co.jp>
2015-05-12 01:32:15 +0900

    add Union-Find Tree by Python3
    Union-Find木を実装した

---
+++ python-library/unionfind.py
@@ -0,0 +1,29 @@
+class UnionFindTree:
+    """Disjoint-Set Data Structure
+       Union-Find Tree"""
+    def __init__(self, n):
+        self.par = list(range(n)) # parent
+        self.rank = [0] * n # depth of tree
+
+    def find(self, x):
+        if self.par[x] == x:
+            return x
+        else:
+            self.par[x] = self.find(self.par[x])
+            return self.par[x]
+
+    def unite(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+
+        if self.rank[x] < self.rank[y]:
+            self.par[x] = y
+        else:
+            self.par[y] = x
+            if self.rank[x] == self.rank[y]:
+                self.rank[x] += 1
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+    
