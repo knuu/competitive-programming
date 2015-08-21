8259139e3a64b265754fbe0fa283f7099064b15a
n_knuu <premier3next@gmail.com>
2015-08-21 16:10:50 +0900

    edit template of C++
    c++テンプレートの修正
    - tupleのためのマクロを追加
    - vectorのためのマクロを追加

--- template/c++.cpp
+++ template/c++.cpp
@@ -5,9 +5,10 @@ typedef long long int ll;
 typedef pair<int, int> P;
 typedef pair<ll, ll> Pll;
 typedef vector<int> Vi;
+typedef tuple<int, int, int> T;
 #define FOR(i,s,x) for(int i=s;i<(int)(x);i++)
 #define REP(i,x) FOR(i,0,x)
-#define PB push_back
+#define ALL(c) c.begin(), c.end()
 #define MP make_pair
 #define DUMP( x ) cerr << #x << " = " << ( x ) << endl
 #define fst first
