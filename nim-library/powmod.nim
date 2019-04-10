c6baa9ecd4488a283e5c6b18d2d5f173fe056ece
knuu <premier3next@gmail.com>
2019-04-11 00:17:37 +0900

    remove contest libraries to move them to other repository

--- nim-library/powmod.nim
+++
@@ -1,8 +0,0 @@
-proc powmod[T](x, k, m: T): T =
-  if k == 0:
-    result = 1
-  elif k mod 2 == 0:
-    let x = powmod(x, k shr 1, m)
-    result = x * x mod m
-  else:
-    result = powmod(x, k-1, m) * x mod m
